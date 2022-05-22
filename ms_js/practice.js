6e146d52997bf07ebb1ee54454755ffa0b3b2e35
Sunbeom <wsbs9805@naver.com>
2022-05-22 22:48:34 +0900

    2강 DOM

---
+++ ms_js/practice.js
@@ -0,0 +1,15 @@
+document.getElementById("lion").style.color = "red";
+
+document.getElementById("tiger").innerHTML = "blue";
+
+document.getElementById("bear").style.color = "green";
+
+const animals = document.getElementById("animals");
+animals.innerHTML += "<li class='animal'>Cat</li>";
+
+document.querySelectorAll(".box")[0].classList.add("purple");
+
+document.querySelectorAll(".box")[0].classList.remove("purple");
+
+document.querySelectorAll(".box")[0].classList.toggle("yellow");
+document.querySelectorAll(".box")[0].classList.toggle("yellow");
