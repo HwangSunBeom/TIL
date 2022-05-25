031b85b1359904b8a33aba40a608bcfd195e50d4
Sunbeom <wsbs9805@naver.com>
2022-05-25 19:09:01 +0900

    이벤트 강의

--- ms_js/practice.js
+++ ms_js/practice.js
@@ -1,15 +1,36 @@
-document.getElementById("lion").style.color = "red";
+// document.getElementById("lion").style.color = "red";
 
-document.getElementById("tiger").innerHTML = "blue";
+// document.getElementById("tiger").innerHTML = "blue";
 
-document.getElementById("bear").style.color = "green";
+// document.getElementById("bear").style.color = "green";
 
-const animals = document.getElementById("animals");
-animals.innerHTML += "<li class='animal'>Cat</li>";
+// const animals = document.getElementById("animals");
+// animals.innerHTML += "<li class='animal'>Cat</li>";
 
-document.querySelectorAll(".box")[0].classList.add("purple");
+// document.querySelectorAll(".box")[0].classList.add("purple");
 
-document.querySelectorAll(".box")[0].classList.remove("purple");
+// document.querySelectorAll(".box")[0].classList.remove("purple");
 
-document.querySelectorAll(".box")[0].classList.toggle("yellow");
-document.querySelectorAll(".box")[0].classList.toggle("yellow");
+// document.querySelectorAll(".box")[0].classList.toggle("yellow");
+// document.querySelectorAll(".box")[0].classList.toggle("yellow");
+
+
+// document.getElementById("btn").addEventListener("click", function(){
+//     console.log("click!!");
+// });
+// var num = 0;
+
+// document.getElementById("add").addEventListener("click", function(){
+//     num++;
+//     document.getElementById("num").innerHTML = num;
+// });
+// document.getElementById("sub").addEventListener("click", function(){
+//     num--;
+//     document.getElementById("num").innerHTML = num;
+// });
+
+document.querySelector(".bar").addEventListener("click", function(){
+    // document.querySelector(".bar").innerHTML = "눌렀어!"
+    // document.querySelector(".newBar").style.display = "block";
+    document.querySelector(".newBar").classList.toggle("show");
+});
