ca929ec50ed5c9beb426406a806abf62e563a9bf
sunbeom <wsbs9805@naver.com>
2022-06-10 02:59:20 +0900

    ms_react_2강

--- ms_react/react-study/src/App.jsx
+++ ms_react/react-study/src/App.jsx
@@ -39,7 +39,31 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faReact } from '@fortawesome/free-brands-svg-icons';
 
 function App() {
-    return <></>;
+    return 
+    <>
+        <MediaDiv>
+            <Header>   
+                <TitleLogoDiv>
+                    <TitleBig>멋사</TitleBig>
+                    <TitleSmall>익명게시판</TitleSmall>
+                </TitleLogoDiv>
+                <SubHeaderDiv>다크모드 버튼</SubHeaderDiv>
+            </Header>
+            <Main>
+                <SlogunBig>HACK YOUR LIFE</SlogunBig>
+                <SlogunSmall>
+                    내 아이디어를 내 손으로 실현한다.
+                </SlogunSmall>
+            </Main>
+            <PostSection>
+                <PostTitleDiv>
+                    <FontAwesomeIcon icon={faArrowsRotate} />
+                    <PostTitle>익명게시판</PostTitle>
+                    <FontAwesomeIcon icon={}
+                </PostTitleDiv>
+            </PostSection>
+        </MediaDiv>
+    </>;
 }
 
 export default App;
