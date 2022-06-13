b524e432eb27c52f29fbf449898a4b4dad3bada0
Sunbeom <wsbs9805@naver.com>
2022-06-13 17:56:38 +0900

    ms_python

---
+++ ms_python/codelion.py
@@ -0,0 +1,18 @@
+import random
+import time
+
+for x in range(30):
+    print(random.choice(["된찌", "피자", "제육볶음", "치킨", "떡볶이"]))
+
+for x in range(30):
+    print(random.choice(["된찌", "피자", "제육볶음", "치킨", "떡볶이"]))
+print("이 문장도 반복되나?")
+
+for x in range(30):
+    print(random.choice(["된찌", "피자", "제육볶음", "치킨", "떡볶이"]))
+    print("이 문장도 반복되나?")
+
+while True:
+    break
+    print(random.choice(["된찌", "피자", "제육볶음", "치킨", "떡볶이"]))
+    time.sleep(1)
