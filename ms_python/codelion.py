2532245cba07c99208836637d587601c776e6bb3
Sunbeom <wsbs9805@naver.com>
2022-06-13 20:55:46 +0900

    Update codelion.py

--- ms_python/codelion.py
+++ ms_python/codelion.py
@@ -1,18 +1,34 @@
 import random
 import time
 
-for x in range(30):
-    print(random.choice(["된찌", "피자", "제육볶음", "치킨", "떡볶이"]))
+# print(random.choice(["된찌", "피자", "제육볶음", "치킨", "떡볶이"]))
 
-for x in range(30):
-    print(random.choice(["된찌", "피자", "제육볶음", "치킨", "떡볶이"]))
-print("이 문장도 반복되나?")
+# for 문
+# for x in range(30):
+#     print(random.choice(["된찌", "피자", "제육볶음", "치킨", "떡볶이"]))
 
-for x in range(30):
-    print(random.choice(["된찌", "피자", "제육볶음", "치킨", "떡볶이"]))
-    print("이 문장도 반복되나?")
+# 들여쓰기
+# for x in range(30):
+#     print(random.choice(["된찌", "피자", "제육볶음", "치킨", "떡볶이"]))
+# print("이 문장도 반복되나?")
 
-while True:
-    break
-    print(random.choice(["된찌", "피자", "제육볶음", "치킨", "떡볶이"]))
-    time.sleep(1)
+# 들여쓰기 2
+# for x in range(30):
+#     print(random.choice(["된찌", "피자", "제육볶음", "치킨", "떡볶이"]))
+#     print("이 문장도 반복되나?")
+
+# while 문
+# while True:
+#     break
+#     print(random.choice(["된찌", "피자", "제육볶음", "치킨", "떡볶이"]))
+#     time.sleep(1)
+
+# 변수 선언
+# lunch = random.choice(["된찌", "피자", "제육볶음", "치킨", "떡볶이"])
+# dinner = random.choice(["김밥", "쫄면", "돈까스"])
+# print(lunch)
+# print(dinner)
+
+# Dictionary
+information = {"고향":"수원", "취미":"영화관람", "좋아하는음식":"국수"}
+print(information)
