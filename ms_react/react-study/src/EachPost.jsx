78128ca855b6c10fefa0d836a883cfa106d27807
Sunbeom <wsbs9805@naver.com>
2022-06-21 13:38:40 +0900

    ms_react_chapter4

--- ms_react/react-study/src/EachPost.jsx
+++ ms_react/react-study/src/EachPost.jsx
@@ -1,26 +1,23 @@
-import React from 'react'
-import {
-    EachPostLi,
-    PostLink,
-    PostRepl,
-} from './styledComponent';
-import {
-    faLocationPin,
-} from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import React from "react";
+import { EachPostLi, PostLink, PostRepl } from "./styledComponent";
+import { faLocationPin } from "@fortawesome/free-solid-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { useNavigate } from "react-router-dom";
 
-function EachPost({title, replCount}){
-    return (
-        <EachPostLi>
-            <div>
-                <FontAwesomeIcon icon={faLocationPin} />
-                <PostLink>
-                    {title}
-                </PostLink>
-            </div>
-            <PostRepl>{replCount}</PostRepl>
-        </EachPostLi>
-    )
+function EachPost({ title, postID }) {
+  const navigate = useNavigate();
+  const goPost = () => {
+    navigate(`${"/post/" + postID}`);
+  };
+
+  return (
+    <EachPostLi onClick={goPost}>
+      <div>
+        <FontAwesomeIcon icon={faLocationPin} />
+        <PostLink>{title}</PostLink>
+      </div>
+    </EachPostLi>
+  );
 }
 
 export default EachPost;
---
+++ ms_react/react-study/src/WriteTitle.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function WriteTitle(props) {
+  return (
+    <PostTitleDiv>
+      <PostTitle>글쓰기</PostTitle>
+    </PostTitleDiv>
+  );
+}
+
+export default WriteTitle;
