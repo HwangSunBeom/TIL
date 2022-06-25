36281411c175d03a35d31e5fd5e989550bb68c37
sunbeom <wsbs9805@naver.com>
2022-06-25 12:55:55 +0900

    ms_react_chapter6-ing

--- ms_react/react-study/src/App.jsx
+++ ms_react/react-study/src/App.jsx
@@ -1,45 +1,42 @@
-import React from 'react'
-import {
-    Main,
-    MediaDiv,
-} from './styledComponent';
-import { darkTheme, GlobalStyles, lightTheme } from './styles';
-import { ThemeProvider } from 'styled-components';
-import Header from './Header';
-import Slogun from './Slogun';
-import ShowPostList from './ShowPostList';
-import Footer from './Footer';
-import EachPost from './EachPost';
-import { useState } from 'react';
-import { Routes, Route } from 'react-router-dom';
-import ShowPost from './ShowPost';
-import WritePost from './WritePost';
+import React from "react";
+import { Main, MediaDiv } from "./styledComponent";
+import { darkTheme, GlobalStyles, lightTheme } from "./styles";
+import { ThemeProvider } from "styled-components";
+import Header from "./Header";
+import Slogun from "./Slogun";
+import ShowPostList from "./ShowPostList";
+import Footer from "./Footer";
+import EachPost from "./EachPost";
+import { useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import ShowPost from "./ShowPost";
+import WritePost from "./WritePost";
+
+const API_URL = "https://reactapitest.pythonanywhere.com/api/";
 
 function App() {
-    const [darkMode, setDarkMode] = useState(true);
-        // const darkMode = false;
-    
-    return (
-        <>
-            <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
-                <GlobalStyles />
-                <MediaDiv>
-                    <Header darkMode={darkMode} setDarkMode={setDarkMode} />
-                    <Main>
-                        <Slogun />
-                        <Routes>
-                            <Route path="/" element={<ShowPostList />}>
-                            </Route>
-                            <Route path="/write" element={<WritePost />}></Route>
-                            <Route path="/post/:postID" element={<ShowPost />}></Route>
-                        </Routes>
-                        
-                    </Main>
-                    <Footer />
-                </MediaDiv>
-            </ThemeProvider>
-        </>
-    )
+  const [darkMode, setDarkMode] = useState(true);
+  // const darkMode = false;
+
+  return (
+    <>
+      <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
+        <GlobalStyles />
+        <MediaDiv>
+          <Header darkMode={darkMode} setDarkMode={setDarkMode} />
+          <Main>
+            <Slogun />
+            <Routes>
+              <Route exact path="/" element={<ShowPostList apiUrl={API_URL} />}></Route>
+              <Route path="/write" element={<WritePost />}></Route>
+              <Route path="/post/:postID" element={<ShowPost />}></Route>
+            </Routes>
+          </Main>
+          <Footer />
+        </MediaDiv>
+      </ThemeProvider>
+    </>
+  );
 }
 
 export default App;
--- ms_react/react-study/src/ShowPostList.jsx
+++ ms_react/react-study/src/ShowPostList.jsx
@@ -21,54 +21,49 @@ import EachPost from "./EachPost";
 import { useEffect, useState, useCallback } from "react";
 import { useNavigate } from "react-router-dom";
 import axios from "axios";
+import { length } from "../node_modules/tailwindcss/src/util/dataTypes 2";
 
-const initialPostList = [
-  { id: 1, title: ", 시사N 대학기자상 취재" },
-  { id: 2, title: "학보, 시사N 대학기자상 취재" },
-  { id: 3, title: "학보, 시사N 대학기자상 취재" },
-];
-
-function ShowPostList() {
+function ShowPostList({ apiUrl }) {
   const [loading, setLoading] = useState(true);
   // const loading = false;
-  const [isPost, setIsPost] = useState(false);
+  // const [isPost, setIsPost] = useState(false);
   // const isPost = false;
   const [postList, setPostList] = useState();
 
-  useEffect(()=>{
-    axios.get('https://reactapitest.pythonanywhere.com/api/list/?page=1&page_size=10')
-    .then(response => {
-      console.log(response);
+  const [page, setPage] = useState(1);
+
+  const [pages, setPages] = useState([]);
+
+  const getPostList = useCallback(()=>{
+    setLoading(true);
+    axios.get(`${apiUrl}list/?page=${page}&page_size=10`).then((response) => {
+
+      const lastPage = Math.ceil(response.data.count / 10);
+      const tempPages = [];
+      for (let i = 1; i <= lastPage; i++) {
+        tempPages.push(i);
+      }
+      setPages(tempPages);
+
+      setPostList(response.data.results);
+      setLoading(false);
     })
-  }, [])
-
-
-  const addPost = useCallback(() => {
-    setPostList((postList) => [
-      ...postList,
-      { id: 4, title: "학보, 시사N 대학기자상 취재" },
-    ]);
-  }, [postList]);
+  });
 
   const navigate = useNavigate();
   const goWrite = () => {
     navigate("/write");
   };
 
-  useEffect(() => {
-    setTimeout(() => {
-      setPostList(initialPostList);
-      setLoading(false);
-    }, 2000);
-  }, []);
+  useEffect(getPostList, [page]);
 
   return (
     <>
       <PostSection>
         <PostTitleDiv>
-          <FontAwesomeIcon onClick={addPost} icon={faArrowsRotate} />
+          <FontAwesomeIcon onClick={getPostList} icon={faArrowsRotate} />
           <PostTitle>익명게시판</PostTitle>
-          <CursorDiv>
+          <CursorDiv onClick={getPostList}>
             <FontAwesomeIcon onClick={goWrite} icon={faPenToSquare} />
           </CursorDiv>
         </PostTitleDiv>
@@ -77,7 +72,7 @@ function ShowPostList() {
             <LoadingDiv>
               <LoadingImg src={loadingIcon} />
             </LoadingDiv>
-          ) : isPost ? (
+          ) : postList.length === 0 ? (
             <LoadingDiv>아직 기록된 글이 없습니다.</LoadingDiv>
           ) : (
             <ul>
@@ -93,11 +88,28 @@ function ShowPostList() {
         </PostListDiv>
       </PostSection>
       <PagingSection>
-        <PagenumberDiv>
+        <PagenumberDiv
+          onClick={() => {
+            if (page > 1) {
+              setPage(page - 1);
+            }
+          }}
+        >
           <FontAwesomeIcon icon={faArrowLeft} />
         </PagenumberDiv>
-        <PagenumberDiv>2</PagenumberDiv>
-        <PagenumberDiv>
+        {pages.map((pageNum) => (
+          <PagenumberDiv key={pageNum} onClick={() => setPage(pageNum)}>
+            {pageNum}
+          </PagenumberDiv>
+        ))}
+
+        <PagenumberDiv
+          onClick={() => {
+            if (pages.length > page) {
+              setPage(page + 1);
+            }
+          }}
+        >
           <FontAwesomeIcon icon={faArrowRight} />
         </PagenumberDiv>
       </PagingSection>
