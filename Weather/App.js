b20ca877ceefdc74a17ed7d98eb8854fded5df30
sunbeom <wsbs9805@naver.com>
2022-07-04 18:52:05 +0900

    flex 연습

--- Weather/App.js
+++ Weather/App.js
@@ -1,24 +1,40 @@
 // import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, StatusBar } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>HELLO I MADE A RN APP!</Text>
-      <StatusBar style="auto" />
+    <View style={{flex: 1,}}>
+      <View style={{flex: 1,backgroundColor:"orange",}}></View>
+      <View style={{flex: 2,backgroundColor:"teal"}}></View>
+      <View style={{flex: 1,backgroundColor:"tomato"}}></View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#F0F0F0',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  // container: {
+  //   flex: 1,
+  //   backgroundColor: '#F0F0F0',
+  //   alignItems: 'center',
+  //   justifyContent: 'center',
+  //   flexDirection:"row"
+  // },
   text: {
     fontSize:28,
     color:"red",
-  }
+  },
+  flexbox: {
+    width:100,
+    height:100,
+    backgroundColor:"red",
+  },
+  background_tomato: {
+    backgroundColor:"tomato"
+  },
+  background_teal: {
+    backgroundColor:"teal"
+  },
+  background_orange:{
+    backgroundColor:"orange"
+  },
 });
