6a90abfa2559a8654f9d675c4ae87b8bca04459c
sunbeom <wsbs9805@naver.com>
2022-07-05 18:53:13 +0900

    RN_WeatherApp

--- Weather/App.js
+++ Weather/App.js
@@ -1,40 +1,146 @@
-// import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  Dimensions,
+  ActivityIndicator,
+} from "react-native";
+import React, { useState, useEffect } from "react";
+import * as Location from "expo-location";
+import { Fontisto } from "@expo/vector-icons";
+
+const { width: SCREEN_WIDTH } = Dimensions.get("window");
+
+const API_KEY = "7ea6037f82d30d1d1d659b057baf8de2";
+
+const icons = {
+  Clouds: "cloudy",
+  Clear: "day-sunny",
+  Atmosphere: "cloudy-gusts",
+  Snow: "snow",
+  Rain: "rains",
+  Drizzle: "rain",
+  Thunderstorm: "lightning",
+};
 
 export default function App() {
+  const [city, setCity] = useState("loading...");
+  const [days, setDays] = useState([]);
+  const [ok, setOk] = useState(true);
+  const getWeather = async () => {
+    const { granted } = await Location.requestForegroundPermissionsAsync();
+    if (!granted) {
+      setOk(false);
+    }
+    const {
+      coords: { latitude, longitude },
+    } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+    const location = await Location.reverseGeocodeAsync(
+      { latitude, longitude },
+      { useGoogleMaps: false }
+    );
+    setCity(location[0].city);
+
+    const response = await fetch(
+      `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=alerts&appid=${API_KEY}&units=metric`
+    );
+    const json = await response.json();
+    setDays(json.daily);
+  };
+  useEffect(() => {
+    getWeather();
+  }, []);
+
   return (
-    <View style={{flex: 1, flexDirection:"row"}}>
-      <View style={{flex: 1,backgroundColor:"orange",}}></View>
-      <View style={{flex: 2,backgroundColor:"teal"}}></View>
-      <View style={{flex: 1,backgroundColor:"tomato"}}></View>
+    <View style={styles.container}>
+      <StatusBar style="light" />
+      <View style={styles.city}>
+        <Text style={styles.cityName}>{city}</Text>
+      </View>
+      <ScrollView
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        indicatorStyle="white"
+        pagingEnabled
+        contentContainerStyle={styles.weather}
+      >
+        {days.length === 0 ? (
+          <View style={{ ...styles.day, alignItems: "center" }}>
+            <ActivityIndicator
+              color="white"
+              style={{ marginTop: 10 }}
+              size="large"
+            />
+          </View>
+        ) : (
+          days.map((day, index) => (
+            <View key={index} style={styles.day}>
+              <View
+                style={{
+                  flexDirection: "row",
+                  alignItems: "center",
+                  width: "100%",
+                  justifyContent: "space-between",
+                }}
+              >
+                <Text style={styles.temp}>
+                  {parseFloat(day.temp.day).toFixed(1)}
+                </Text>
+                <Fontisto
+                  name={icons[day.weather[0].main]}
+                  size={68}
+                  color="white"
+                />
+              </View>
+              <Text style={styles.description}>{day.weather[0].main}</Text>
+              <Text style={styles.tinyText}>{day.weather[0].description}</Text>
+            </View>
+          ))
+        )}
+      </ScrollView>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  // container: {
-  //   flex: 1,
-  //   backgroundColor: '#F0F0F0',
-  //   alignItems: 'center',
-  //   justifyContent: 'center',
-  //   flexDirection:"row"
-  // },
-  text: {
-    fontSize:28,
-    color:"red",
+  container: {
+    flex: 1,
+    backgroundColor: "tomato",
   },
-  flexbox: {
-    width:100,
-    height:100,
-    backgroundColor:"red",
+  city: {
+    flex: 1.2,
+    justifyContent: "center",
+    alignItems: "center",
   },
-  background_tomato: {
-    backgroundColor:"tomato"
+  cityName: {
+    fontSize: 68,
+    fontWeight: "500",
+    color: "white",
   },
-  background_teal: {
-    backgroundColor:"teal"
+  weather: {},
+  day: {
+    alignItems: "flex-start",
+    width: SCREEN_WIDTH,
+    paddingHorizontal: 20,
   },
-  background_orange:{
-    backgroundColor:"orange"
+  temp: {
+    marginTop: 50,
+    fontSize: 100,
+    fontWeight: "600",
+    color: "white",
+  },
+  description: {
+    fontSize: 30,
+    marginTop: -10,
+    color: "white",
+    fontWeight: "500",
+  },
+  tinyText: {
+    fontSize: 25,
+    marginTop: -5,
+    color:"white",
+    fontWeight:"500",
   },
 });
