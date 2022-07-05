5f78d572f0cd8c884663f6ed7f58a567bdfefe7e
sunbeom <wsbs9805@naver.com>
2022-07-05 11:00:50 +0900

    RN_flex_test

--- Weather/App.js
+++ Weather/App.js
@@ -3,7 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
   return (
-    <View style={{flex: 1,}}>
+    <View style={{flex: 1, flexDirection:"row"}}>
       <View style={{flex: 1,backgroundColor:"orange",}}></View>
       <View style={{flex: 2,backgroundColor:"teal"}}></View>
       <View style={{flex: 1,backgroundColor:"tomato"}}></View>
