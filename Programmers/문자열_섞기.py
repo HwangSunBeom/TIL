7685e7cbe00075bbd3bde8d243e8111eb2a3d6f7
Sunbeom <wsbs9805@naver.com>
2024-02-09 16:14:53 +0900

    프로그래머스 3일차 문자열섞기

---
+++ Programmers/문자열_섞기.py
@@ -0,0 +1,11 @@
+def solution(str1, str2):
+    answer = ''
+    j, k = 0, 0
+    for i in range(len(str1) + len(str2)) :
+        if i%2 == 0 :
+            answer += str1[j]
+            j += 1
+        else :
+            answer += str2[k]
+            k += 1
+    return answer
