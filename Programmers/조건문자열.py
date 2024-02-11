4779dca25570bd60b1e02c76e194baca772cbb6f
Sunbeom <wsbs9805@naver.com>
2024-02-11 15:45:50 +0900

    프로그래머스 조건문자열

---
+++ Programmers/조건문자열.py
@@ -0,0 +1,11 @@
+def solution(ineq, eq, n, m):
+    answer = 0
+    if ineq == '<' and eq == '=' :    
+        answer = int(bool(n <= m))
+    elif ineq == '>' and eq == '=' :    
+        answer = int(bool(n >= m))
+    elif ineq == '<' and eq == '!' :
+        answer = int(bool(n < m))
+    else :
+        answer = int(bool(n > m))
+    return answer
