b4e13e2945c6665c6d60f25aea954b09c9cec1f6
Sunbeom <wsbs9805@naver.com>
2024-02-20 23:42:20 +0900

    주사위 게임 3 풀이중

---
+++ Programmers/주사위_게임_3.py
@@ -0,0 +1,14 @@
+def solution(a, b, c, d):
+    answer = 0
+    dice = [a, b, c, d]
+    count = {x : dice.count(x) for x in dice  }
+    if len(count) == 1 :
+        return a * 1111
+    elif count.values() == 3 :
+        for key, value in count.items() :
+            if value = 3 :
+                p = key
+            else :
+                q = key
+        return 
+    return answer
