596f08fdcd3aa587e717607147618ca6ef6b4753
Sunbeom <wsbs9805@naver.com>
2024-03-07 23:54:28 +0900

    프로그래머스 배열 조각하기

---
+++ Programmers/배열_조각하기.py
@@ -0,0 +1,10 @@
+def solution(arr, query):
+    answer = []
+    index = 0
+    for num in query:
+        if index % 2 :
+            arr = arr[num:]
+        else :
+            arr = arr[:num+1]
+        index += 1
+    return arr
