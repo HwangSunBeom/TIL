d6bbb749189cf2fca5cf8f135a8a403914f1b327
Sunbeom <wsbs9805@naver.com>
2024-03-20 23:38:48 +0900

    프로그래머스 A 강조하기

---
+++ Programmers/A_강조하기.py
@@ -0,0 +1,10 @@
+def solution(myString):
+    answer = ''
+    for str in myString :
+        if str == 'a' :
+            answer += str.upper()
+        elif str == 'A' :
+            answer += str
+        else :
+            answer += str.lower()
+    return answer
