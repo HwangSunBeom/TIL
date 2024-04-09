754cde476a5e63c0a3c57187edf0ba11bb100c94
Sunbeom <wsbs9805@naver.com>
2024-04-09 21:03:01 +0900

    프로그래머스 기능개발

---
+++ Programmers/기능개발.py
@@ -0,0 +1,16 @@
+import math
+
+def solution(progresses, speeds):
+    answer = []
+    point = math.ceil((100 - progresses[0]) / speeds[0])
+    temp = {point : 0}
+    for index, prog in enumerate(progresses) :
+        day = math.ceil((100 - prog) / speeds[index])
+        if day <= point :
+            temp[point] += 1
+        elif day in temp :
+            temp[day] += 1
+        else :
+            point = day
+            temp[day] = 1
+    return [x for x in temp.values()]
