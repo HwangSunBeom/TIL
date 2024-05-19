dbfe2d0e28d463dea59bc161e443758c73674d9b
Sunbeom <wsbs9805@naver.com>
2024-05-19 22:41:59 +0900

    SWEA 수도 요금 경쟁

---
+++ SWEA/수도_요금_경쟁.py
@@ -0,0 +1,11 @@
+T = int(input())
+for test_case in range(1, T + 1):
+    P, Q, R, S, W = map(int, input().split())
+    A_cost = W * P
+    if W > R:
+        B_cost = Q + ((W - R) * S)
+    else:
+        B_cost = Q
+
+    answer = min(A_cost, B_cost)
+    print(f"#{test_case} {answer}")
