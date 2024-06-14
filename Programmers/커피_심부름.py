e04fd6db1cf565795a65cd8ff68292a7340ebd08
Sunbeom <wsbs9805@naver.com>
2024-06-14 19:52:37 +0900

    프로그래머스 커피 심부름

---
+++ Programmers/커피_심부름.py
@@ -0,0 +1,8 @@
+def solution(order):
+    answer = 0
+    for menu in order :
+        if 'latte' in menu :
+            answer += 5000
+        else :
+            answer += 4500
+    return answer
