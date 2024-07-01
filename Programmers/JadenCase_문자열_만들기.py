7cd8f7f766365169f10d9bd82ec817dff0432433
Sunbeom <wsbs9805@naver.com>
2024-07-01 21:52:59 +0900

    프로그래머스 JadenCase 문자열 만들기

---
+++ Programmers/JadenCase_문자열_만들기.py
@@ -0,0 +1,8 @@
+def solution(s):
+    answer = ''
+    for i, str in enumerate(s) :
+        if i == 0 or (i != 0 and s[i-1]== ' ') :
+            answer += str.upper()
+        else :
+            answer += str.lower()
+    return answer
