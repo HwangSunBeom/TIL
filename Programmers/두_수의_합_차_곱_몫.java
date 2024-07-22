36eae963c695d6b1ec56a03dac2e66754f789cec
Sunbeom <wsbs9805@naver.com>
2024-07-22 22:26:26 +0900

    프로그래머스 사칙 연산

---
+++ Programmers/두_수의_합_차_곱_몫.java
@@ -0,0 +1,25 @@
+
+// class Solution {
+//     public int solution(int num1, int num2) {
+//         return num1 + num2;
+//     }
+// }
+
+// class Solution {
+//     public int solution(int num1, int num2) {
+//         return num1 - num2;
+//     }
+// }
+
+// class Solution {
+//     public int solution(int num1, int num2) {
+//         return num1 * num2;
+//     }
+// }
+
+
+class Solution {
+    public int solution(int num1, int num2) {
+        return num1 / num2;
+    }
+}
