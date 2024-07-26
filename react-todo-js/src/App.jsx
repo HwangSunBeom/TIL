9c8fa201f6ff908403a62e6fa5a6e11f7249bb42
Sunbeom <wsbs9805@naver.com>
2024-07-26 11:49:42 +0900

    프로젝트 기본구성

--- react-todo-js/src/App.jsx
+++ react-todo-js/src/App.jsx
@@ -1,17 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
 
 function App() {
-
   return (
     <>
-      <h1 className="tesxt-3xl font-bold underline">
-        Todos
-      </h1>
+      <DefaultLayout>
+        <header>
+          <div className="flex justify-center">
+            <a to="/" className="flex">
+              <h1 className="py-8 text-red-200 max-w-max text-7xl">todos</h1>
+              <img
+                src="https://raw.githubusercontent.com/Tarikul-Islam-Anik/Animated-Fluent-Emojis/master/Emojis/Smilies/Cat%20with%20Wry%20Smile.png"
+                alt="Cat with Wry Smile"
+                width="50"
+                height="50"
+              />
+            </a>
+          </div>
+        </header>
+        <section className="max-w-xl m-4 mx-auto">
+          <TodoHeader />
+          <TodoBody />
+        </section>
+      </DefaultLayout>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
