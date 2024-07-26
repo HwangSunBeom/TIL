fd28360a0f1cd0bd9437c891986d462b693f46eb
Sunbeom <wsbs9805@naver.com>
2024-07-26 14:24:49 +0900

    Todo 등록기능 구현

--- react-todo-js/src/App.jsx
+++ react-todo-js/src/App.jsx
@@ -29,6 +29,16 @@ function App() {
 
   const [todos, setTodos] = useState(dummyTodos);
 
+  const addTodoHandler = ({title, summary, category}) => {
+    const newTodo = {
+      id: todos.length + 1,
+      title,
+      summary,
+      category,
+    }
+    const updatedTodos = [...todos, newTodo];
+    setTodos(updatedTodos);
+  }
 
   return (
     <>
@@ -47,7 +57,7 @@ function App() {
           </div>
         </header>
         <section className="max-w-xl m-4 mx-auto">
-          <TodoHeader />
+          <TodoHeader onAdd={addTodoHandler} />
           <TodoBody todos={todos} />
         </section>
       </DefaultLayout>
--- react-todo-js/src/components/todos/TodoHeader.jsx
+++ react-todo-js/src/components/todos/TodoHeader.jsx
@@ -1,16 +1,35 @@
 // index.html같은 곳에 작성했던 방식처럼 동일하게
-import TodoFilter from './TodoFilter'
+import { createPortal } from "react-dom";
+import Modal from "@/components/ui/Modal";
+import TodoForm from "./TodoForm";
+import { useState } from "react";
+import TodoFilter from "./TodoFilter";
 
 // 함수형 컴포넌트(TodoHeader)
-const TodoHeader = () => {
-    return (
-        <div className="flex items-center justify-between mb-2" id="task-control">
-        <button className="px-6 py-2 font-semibold text-gray-100 bg-gray-800 border-none rounded cursor-pointer"
-                data-cy="add-todo-button">Add Todo
-        </button>
-        <TodoFilter />
-      </div> 
-    )
-}
+const TodoHeader = ({ onAdd }) => {
+  const [open, setOpen] = useState(false);
+  const closeModel = () => setOpen(false);
+  const openModel = () => setOpen(true);
+
+  return (
+    <div className="flex items-center justify-between mb-2" id="task-control">
+      <button
+        className="px-6 py-2 font-semibold text-gray-100 bg-gray-800 border-none rounded cursor-pointer"
+        data-cy="add-todo-button"
+        onClick={openModel}
+      >
+        Add Todo
+      </button>
+      {open &&
+        createPortal(
+          <Modal onClose={closeModel}>
+            <TodoForm onClose={closeModel} onAdd={onAdd} />
+          </Modal>,
+          document.body
+        )}
+      <TodoFilter />
+    </div>
+  );
+};
 
 export default TodoHeader;
